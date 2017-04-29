7e410675ff5996c2925a34d3db090a29a01abadb
Al <jinto12@gmail.com>
2017-04-28 18:31:51 -0700

    Update db_config.js
    config to connect to heroku

--- DAO/db_config.js
+++ DAO/db_config.js
@@ -3,11 +3,18 @@
  */
 var db_config = {
     config:{
-        host:'localhost',
-        user:'root',
-        password:'123456',
-        database:'production' //'termproj_dev'
+        host:'us-cdbr-iron-east-04.cleardb.net',
+        user:'be3ec741b80552',
+        password:'ee722cd2',
+        database:'heroku_95b803e0fd13b34'
+       
     }
 };
 
 exports.db_config=db_config;
+
+/*
+ host:'localhost',
+        user:'root',
+        password:'123456',
+        database:'production' //'termproj_dev'*/
